dcb50df5e313da0e07a154479b033c01891c1906
annakomarovabs <anya.komarova2702@gmail.com>
2021-12-02 14:31:49 +0300

    Add files via upload

---
+++ morse.py
@@ -0,0 +1,69 @@
+"""Morse Code Translator"""
+
+import doctest
+
+
+LETTER_TO_MORSE = {
+    'A': '.-', 'B': '-...', 'C': '-.-.',
+    'D': '-..', 'E': '.', 'F': '..-.',
+    'G': '--.', 'H': '....', 'I': '..',
+    'J': '.---', 'K': '-.-', 'L': '.-..',
+    'M': '--', 'N': '-.', 'O': '---',
+    'P': '.--.', 'Q': '--.-', 'R': '.-.',
+    'S': '...', 'T': '-', 'U': '..-',
+    'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..', '1': '.----',
+    '2': '..---', '3': '...--', '4': '....-',
+    '5': '.....', '6': '-....', '7': '--...',
+    '8': '---..', '9': '----.', '0': '-----',
+    ',': '--..--', '.': '.-.-.-', '?': '..--..',
+    '/': '-..-.', '-': '-....-', '(': '-.--.', ')': '-.--.-',
+    ' ': '/'
+}
+
+MORSE_TO_LETTER = {
+    morse: letter
+    for letter, morse in LETTER_TO_MORSE.items()
+}
+
+
+def encode(message: str) -> str:
+    """
+    Кодирует строку в соответсвие с таблицей азбуки Морзе
+
+    >>> doctest.ELLIPSIS_MARKER = '!ignore!'
+    >>> encode('HELLO, WORLD')
+    '.... . .-.. .-.. --- --..-- / .-- --- .-. .-.. -..'
+    >>> encode('HELLO, WORLD') #doctest: +ELLIPSIS
+    '.... !ignore! -..'
+    >>> encode('HELLO, WORLD') #doctest: +NORMALIZE_WHITESPACE
+    '.... . .-.. .-.. --- --..-- /  .-- --- .-. .-.. -..'
+    >>> encode('SOS')
+    '... --- ...'
+    >>> encode('Привет, Мир')
+    """
+    encoded_signs = [
+        LETTER_TO_MORSE[letter] for letter in message
+    ]
+
+    return ' '.join(encoded_signs)
+
+
+def decode(morse_message: str) -> str:
+    """
+    Декодирует строку из азбуки Морзе в английский
+    """
+    decoded_letters = [
+        MORSE_TO_LETTER[letter] for letter in morse_message.split()
+    ]
+
+    return ''.join(decoded_letters)
+
+
+if __name__ == '__main__':
+    # morse_msg = '-- .- .. -....- .--. -.-- - .... --- -. -....- ..--- ----- .---- ----.'
+    # decoded_msg = decode(morse_msg)
+    # print(decoded_msg)
+    # print(encode('HELLO, WORLD'))
+    # assert morse_msg == encode(decoded_msg)
+    doctest.testmod()
